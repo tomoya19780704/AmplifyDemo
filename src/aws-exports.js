c688a4a0fd8fd3f65321e92c1be34873a3666f2e
ishida <ishida@ishidanoMacBook.local>
2021-03-28 00:42:48 +0900

    add s3

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://fctbvbpdefekvdv4ri2nqq7tsu.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-igjktuhlwfhwnltitjsqw2lk6i"
+    "aws_appsync_apiKey": "da2-igjktuhlwfhwnltitjsqw2lk6i",
+    "aws_user_files_s3_bucket": "amplify-demo-tomoya74133401-staging",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
